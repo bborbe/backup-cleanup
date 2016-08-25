d901b95c2344ea1ab60d812736e6f9979b646913
Benjamin Borbe <bborbe@rocketnews.de>
2016-08-25 19:51:03 +0200

    refactoring

--- bin/backup_cleanup_cron/backup_cleanup_cron_test.go
+++ bin/backup_cleanup_cron/backup_cleanup_cron_test.go
@@ -15,7 +15,7 @@ func TestDoFail(t *testing.T) {
 	err := do(writer, func(directory string, match string, keepAmount int) error {
 		return nil
 	}, "", "", -1, time.Minute, false, "/tmp/lock")
-	if err = AssertThat(err, NotNilValue()); err != nil {
+	if err := AssertThat(err, NotNilValue()); err != nil {
 		t.Fatal(err)
 	}
 }
@@ -25,7 +25,7 @@ func TestDoSuccess(t *testing.T) {
 	err := do(writer, func(directory string, match string, keepAmount int) error {
 		return nil
 	}, "/tmp", "backup", 5, time.Minute, true, "/tmp/lock")
-	if err = AssertThat(err, NilValue()); err != nil {
+	if err := AssertThat(err, NilValue()); err != nil {
 		t.Fatal(err)
 	}
 }
